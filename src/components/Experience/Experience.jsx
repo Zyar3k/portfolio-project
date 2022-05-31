6391b025c9e2f69f74a20c655585e1cfd42a98b9
Arkadiusz Zygan <a.zygan@gmail.com>
2022-05-31 22:59:53 +0200

    Add experience component

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,7 +1,65 @@
+import { BsPatchCheckFill } from "react-icons/bs";
+
 import "./Experience.scss";
 
+const frontData = [
+  "HTML",
+  "CSS",
+  "Sass",
+  "Tailwind",
+  "Styled-components",
+  "Bootstrap",
+  "Javascript",
+  "Typescript",
+  "React",
+  "Vue",
+];
+
+const backData = [
+  "Node JS",
+  "MongoDB",
+  "Express.js",
+  "JWT",
+  "Socket.io",
+  "Postman",
+  "Git",
+];
+
 const Experience = () => {
-  return <section id="experience">Experience</section>;
+  return (
+    <section id="experience">
+      <h5>Mam doświadczenie w pracy z... </h5>
+      <h2>Moje umiejętności</h2>
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            {frontData.map((item, index) => (
+              <article key={index} className="experience__details">
+                <BsPatchCheckFill className="experience__details-icon" />
+                <div>
+                  <h4>{item}</h4>
+                </div>
+              </article>
+            ))}
+          </div>
+        </div>
+        <div className="experience__backend">
+          <h3>Backend Development</h3>
+          <div className="experience__content">
+            {backData.map((item, index) => (
+              <article key={index} className="experience__details">
+                <BsPatchCheckFill className="experience__details-icon" />
+                <div>
+                  <h4>{item}</h4>
+                </div>
+              </article>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Experience;
